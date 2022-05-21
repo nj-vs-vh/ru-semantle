49cf1bd56db3a2354154588c4be9e92c50571021
Igor Vaiman <igor.vaiman@inten.to>
2022-05-21 17:31:43 +0400

    winning word is not rendered separately

--- frontend/src/components/NewGuessedWord.svelte
+++ frontend/src/components/NewGuessedWord.svelte
@@ -7,19 +7,18 @@
     import { createEventDispatcher } from "svelte";
     import type { WordGuess } from "../types";
 
-    const dispatch = createEventDispatcher<{ successfulWordGuess: { wg: WordGuess } }>();
+    const dispatch =
+        createEventDispatcher<{ successfulWordGuess: { wg: WordGuess } }>();
 
     export let guessedWord: string;
 
-    $: wordGuessPromise = guessWord(guessedWord).then(
-        (wgr) => {
-            console.log(wgr);
-            if (typeof wgr !== "string") {
-                dispatch("successfulWordGuess", { wg: wgr });
-            }
-            return wgr;
+    $: wordGuessPromise = guessWord(guessedWord).then((wgr) => {
+        console.log(wgr);
+        if (typeof wgr !== "string") {
+            dispatch("successfulWordGuess", { wg: wgr });
         }
-    );
+        return wgr;
+    });
 </script>
 
 {#await wordGuessPromise}
@@ -27,7 +26,7 @@
 {:then wordGuessResult}
     {#if typeof wordGuessResult === "string"}
         <Error errorMessage={wordGuessResult} />
-    {:else}
+    {:else if wordGuessResult.rating != 1}
         <WordTable>
             <WordRow wordGuess={wordGuessResult} />
         </WordTable>
