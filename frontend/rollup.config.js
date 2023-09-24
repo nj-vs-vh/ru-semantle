d9eb6abbdfad61120b337fd551d8ca7b454ddc6f
Igor Vaiman <gosha.vaiman@gmail.com>
2023-09-24 11:37:10 +0300

    fix access log

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,96 +1,108 @@
-import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
-import css from 'rollup-plugin-css-only';
-import replace from '@rollup/plugin-replace';
+import svelte from "rollup-plugin-svelte";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import css from "rollup-plugin-css-only";
+import replace from "@rollup/plugin-replace";
+
+let baseUrl = process.env.GIT_TAG;
+if (!baseUrl) {
+  console.error("Base URL not specified");
+  throw new Error();
+}
+console.info(`Base URL: ${baseUrl}`);
 
 let version = process.env.GIT_TAG;
 if (!version) {
-	console.warn("No GIT_TAG environment variable found");
-	version = "development";
+  console.warn("No GIT_TAG environment variable found");
+  version = "development";
 }
 console.log(`Building with version ${version}`);
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-	let server;
+  let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "start", "--", "--dev"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    },
+  };
 }
 
 export default {
-	input: 'src/main.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		replace({
-			buildTimeReplacedVersion: version,
-			buildTimeReplacedIsProduction: production,
-			preventAssignment: true,
-		}),
-		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
-			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production
-			}
-		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+  input: "src/main.ts",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    replace({
+      buildTimeReplacedVersion: version,
+      buildTimeReplacedIsProduction: baseUrl,
+      buildTimeReplacedBaseUrl: production,
+      preventAssignment: true,
+    }),
+    svelte({
+      preprocess: sveltePreprocess({ sourceMap: !production }),
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+      },
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({ output: "bundle.css" }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-		typescript({
-			sourceMap: !production,
-			inlineSources: !production
-		}),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
+    }),
+    commonjs(),
+    typescript({
+      sourceMap: !production,
+      inlineSources: !production,
+    }),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
 };
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,120 +1,122 @@
 import type { GameMetadata, WordGuess, WordGuessResult } from "./types";
 import { sleep, toIsoStringTz } from "./utils";
 
-
-// @ts-ignore
-const baseUrl = buildTimeReplacedIsProduction ? "https://ru-semantle.herokuapp.com" : "http://localhost:8088";
+let baseUrl: string;
+try {
+  // @ts-ignore
+  baseUrl = buildTimeReplacedBaseUrl;
+} catch {
+  // @ts-ignore
+  baseUrl = "http://localhost:8088";
+}
 
 const networkErrorMessage = "Ошибка подключения, попробуйте обновить страницу!";
 
-
-export async function guessWord(word: string, guessIdx?: number): Promise<WordGuessResult> {
-    // await sleep(5);   // for loading spinner test
-    console.log(`Guessing "${word}" (${baseUrl})...`)
-    try {
-        const resp = await fetch(
-            `${baseUrl}/guess`,
-            {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ guess: word })
-            }
-        )
-        const respText = await resp.text();
-        if (resp.ok) {
-            const wg: WordGuess = JSON.parse(respText);
-            wg.guessedAt = toIsoStringTz(new Date());
-            wg.idx = guessIdx;
-            return wg;
-        } else {
-            return respText;
-        }
-    } catch (error) {
-        console.warn(`Error fetching data: ${error}`)
-        return networkErrorMessage;
+export async function guessWord(
+  word: string,
+  guessIdx?: number
+): Promise<WordGuessResult> {
+  // await sleep(5);   // for loading spinner test
+  console.log(`Guessing "${word}" (${baseUrl})...`);
+  try {
+    const resp = await fetch(`${baseUrl}/guess`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ guess: word }),
+    });
+    const respText = await resp.text();
+    if (resp.ok) {
+      const wg: WordGuess = JSON.parse(respText);
+      wg.guessedAt = toIsoStringTz(new Date());
+      wg.idx = guessIdx;
+      return wg;
+    } else {
+      return respText;
     }
+  } catch (error) {
+    console.warn(`Error fetching data: ${error}`);
+    return networkErrorMessage;
+  }
 }
-
 
 export async function getRandomWords(): Promise<string[] | null> {
-    console.log(`Fetching random words (${baseUrl})...`)
-    try {
-        const resp = await fetch(`${baseUrl}/random-words`)
-        const respText = await resp.text();
-        if (resp.ok) {
-            console.log("OK")
-            return JSON.parse(respText);
-        } else {
-            console.log(`Error: ${respText}`)
-            return null;
-        }
-    } catch (error) {
-        console.warn(`Error fetching data: ${error}`)
-        return null;
+  console.log(`Fetching random words (${baseUrl})...`);
+  try {
+    const resp = await fetch(`${baseUrl}/random-words`);
+    const respText = await resp.text();
+    if (resp.ok) {
+      console.log("OK");
+      return JSON.parse(respText);
+    } else {
+      console.log(`Error: ${respText}`);
+      return null;
     }
+  } catch (error) {
+    console.warn(`Error fetching data: ${error}`);
+    return null;
+  }
 }
-
 
 export async function getMetadata(): Promise<GameMetadata | string> {
-    // await sleep(5);   // for loading spinner test
-    // return "Example error message";  // for error display test
-    console.log(`Fetching metadata (${baseUrl})...`)
-    try {
-        const resp = await fetch(`${baseUrl}/metadata`)
-        const respText = await resp.text();
-        if (resp.ok) {
-            console.log("OK")
-            return JSON.parse(respText);
-        } else {
-            console.log(`Error: ${respText}`)
-            return respText;
-        }
-    } catch (error) {
-        console.warn(`Error fetching data: ${error}`)
-        return networkErrorMessage;
+  // await sleep(5);   // for loading spinner test
+  // return "Example error message";  // for error display test
+  console.log(`Fetching metadata (${baseUrl})...`);
+  try {
+    const resp = await fetch(`${baseUrl}/metadata`);
+    const respText = await resp.text();
+    if (resp.ok) {
+      console.log("OK");
+      return JSON.parse(respText);
+    } else {
+      console.log(`Error: ${respText}`);
+      return respText;
     }
+  } catch (error) {
+    console.warn(`Error fetching data: ${error}`);
+    return networkErrorMessage;
+  }
 }
 
-
-export async function getHint(currentBestRating: number | null): Promise<WordGuess | null> {
-    console.log(`Fetching hint (better than ${currentBestRating})...`)
-    try {
-        const resp = await fetch(`${baseUrl}/hint`, {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ current_best_rating: currentBestRating })
-        })
-        const respText = await resp.text();
-        if (resp.ok) {
-            console.log("OK")
-            return JSON.parse(respText);
-        } else {
-            console.log(`Error: ${respText}`)
-            return null;
-        }
-    } catch (error) {
-        console.warn(`Error fetching data: ${error}`)
-        return null;
+export async function getHint(
+  currentBestRating: number | null
+): Promise<WordGuess | null> {
+  console.log(`Fetching hint (better than ${currentBestRating})...`);
+  try {
+    const resp = await fetch(`${baseUrl}/hint`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ current_best_rating: currentBestRating }),
+    });
+    const respText = await resp.text();
+    if (resp.ok) {
+      console.log("OK");
+      return JSON.parse(respText);
+    } else {
+      console.log(`Error: ${respText}`);
+      return null;
     }
+  } catch (error) {
+    console.warn(`Error fetching data: ${error}`);
+    return null;
+  }
 }
-
 
 export async function getTopWords(): Promise<WordGuess[] | null> {
-    // await sleep(5);   // for loading spinner test
-    // return "Example error message";  // for error display test
-    console.log(`Fetching top words (${baseUrl})...`)
-    try {
-        const resp = await fetch(`${baseUrl}/give-up`)
-        const respText = await resp.text();
-        if (resp.ok) {
-            console.log("OK")
-            return JSON.parse(respText);
-        } else {
-            console.log(`Error: ${respText}`)
-            return null;
-        }
-    } catch (error) {
-        console.warn(`Error fetching data: ${error}`)
-        return null;
+  // await sleep(5);   // for loading spinner test
+  // return "Example error message";  // for error display test
+  console.log(`Fetching top words (${baseUrl})...`);
+  try {
+    const resp = await fetch(`${baseUrl}/give-up`);
+    const respText = await resp.text();
+    if (resp.ok) {
+      console.log("OK");
+      return JSON.parse(respText);
+    } else {
+      console.log(`Error: ${respText}`);
+      return null;
     }
+  } catch (error) {
+    console.warn(`Error fetching data: ${error}`);
+    return null;
+  }
 }
