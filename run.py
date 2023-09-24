30086c39b21aa32390cfd24508c83e071a68a953
Igor Vaiman <gosha.vaiman@gmail.com>
2023-09-24 11:37:25 +0300

    fix

--- run.py
+++ run.py
@@ -4,10 +4,6 @@ import logging
 from backend.server import create_app
 from backend import config
 
-run_kwargs = dict()
-if config.IS_PROD:
-    run_kwargs["access_log"] = None
-
 logging.basicConfig(level=logging.INFO)
 config.log_config()
-web.run_app(create_app(), port=config.PORT, **run_kwargs)
+web.run_app(create_app(), port=config.PORT)
